7eec65aed8a268c7581f90a3614497a82b77ad12
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-01-28 23:29:20 +0530

    Pong 🏓

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,13 @@
 const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const ms = require("ms");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Returns the bot's latency!"),
   
-    run: async (client, interaction) => {
-      interaction.reply(`Pong 🏓`)
+    run: async (client, interaction, message) => {
+      interaction.reply(`Pong! API Latency is ${Math.round(client.ws.ping)}ms 🛰️, Last heartbeat calculated ${ms(Date.now() - client.ws.shards.first().lastPingTimestamp, { long: true })} ago`)
     }
  };
