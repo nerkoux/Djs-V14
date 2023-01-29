38ef9cce938622ac4368b7b9163e612d7feeee5b
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-01-29 19:39:35 +0530

    Changes in Config 🔧

--- configuration.js
+++ configuration.js
@@ -3,11 +3,11 @@
 module.exports = {
     prefix: ",",
     owner: ["833404675692036166"],
-    token: "MTA1Njk2NTM1MjA5NDk2NTc5MQ.GLKoDT.KPyAUxePia8--xt6fxoWa8yucD6kFjXkSbTqV4",
-    dbd_license: "66998100-0844-4137-9e0b-3384bf0efac7", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
-    client_id: "1056965352094965791", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    client_secret: "0wSp1OASUjxZ0kiCYcSH5ZymzSD16G2n", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    port: 80,
-    domain: "http://localhost",
+    token: "token",
+    dbd_license: "license", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
+    client_id: "client_id", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    client_secret: "client_Secret", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    port: port,
+    domain: "domain",
     redirectUri: "/discord/callback",
   }
