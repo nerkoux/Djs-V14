5c8c332d47cb9cd23cf3bf0f60a4813dd842bb54
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-01-29 19:35:49 +0530

    A lot change in the configs

--- configuration.js
+++ configuration.js
@@ -2,10 +2,12 @@
 
 module.exports = {
     prefix: ",",
-    owner: "owner_id",
-    token: "token",
-    dbd_license: "license_id", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
-    client_id: "client_id", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    client_secret: "client_secret", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    redirect_uri: "redirect_uri", // YOU HAVE TO ADD REDIRECT URI ON THI PAGE - https://discord.com/developers/applications/{client_id}/oauth2/general
+    owner: ["833404675692036166"],
+    token: "MTA1Njk2NTM1MjA5NDk2NTc5MQ.GLKoDT.KPyAUxePia8--xt6fxoWa8yucD6kFjXkSbTqV4",
+    dbd_license: "66998100-0844-4137-9e0b-3384bf0efac7", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
+    client_id: "1056965352094965791", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    client_secret: "0wSp1OASUjxZ0kiCYcSH5ZymzSD16G2n", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    port: 80,
+    domain: "http://localhost",
+    redirectUri: "/discord/callback",
   }
