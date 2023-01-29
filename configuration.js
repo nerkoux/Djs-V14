037265ea32c3f20e1dc91d7f7fd632f6641648b7
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-01-29 12:45:09 +0530

    Few Updates in Configuration 🙌

--- configuration.js
+++ configuration.js
@@ -1,5 +1,11 @@
+/* --- CONFIGURATION --- */
+
 module.exports = {
     prefix: ",",
     owner: "833404675692036166",
-    token: "Token"
+    token: "MTA1Njk2NTM1MjA5NDk2NTc5MQ.GAPx_g.bVEWdSd_v7T8N9JqKCrPLWnX3Xk3dXmzdt8Ccc",
+    dbd_license: "66998100-0844-4137-9e0b-3384bf0efac7", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
+    client_id: "1056965352094965791", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    client_secret: "0wSp1OASUjxZ0kiCYcSH5ZymzSD16G2n", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    redirect_uri: "http://localhost/discord/callback", // YOU HAVE TO ADD REDIRECT URI ON THI PAGE - https://discord.com/developers/applications/{client_id}/oauth2/general
   }
