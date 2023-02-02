ed350cece5599377eab64a9a5382ef8595d137b3
Nerko Dev <akshatmehta_14434@mpsjaipur.com>
2023-02-02 08:50:27 +0530

    Quick Configs 🎉

--- configuration.js
+++ configuration.js
@@ -2,12 +2,12 @@
 
 module.exports = {
     prefix: ",",
-    owner: ["833404675692036166"],
-    token: "ODYxNTc1MDQzMTg3ODY3NjQ4.GZA3fa.pDH0bUwGqtVBPthCh2Bn-FTMZffknnXJ4BgbgO",
-    dbd_license: "434ff479-9da7-4d5e-9030-0fe22279e5f7", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
-    client_id: "1056965352094965791", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    client_secret: "N4WWwrZKSDgyVJzoJs58d_dmziyUCVxW", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
-    port: 80,
-    domain: "http://localhost",
+    owner: ["ownerid"],
+    token: "token",
+    dbd_license: "license", // YOU CAN GET THIS FROM HERE - https://assistantscenter.com/discord-dashboard/v2
+    client_id: "client_id", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    client_secret: "client_Secret", // YOU CAN GET THIS FROM HERE - https://discord.com/developers/applications
+    port: port,
+    domain: "domain",
     redirectUri: "/discord/callback",
   }
